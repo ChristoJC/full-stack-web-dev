9afd25073b428d39f235b6f53b6666e996b0d9df
ChristoJC <christocheng2238@gmail.com>
2022-03-11 18:03:49 +0000

    apllied css styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,9 +14,23 @@
         margin-bottom: 0.5rem;
     }
 
-    .new input{
-        
-    }
+    .new input {
+    font-size: 28px;
+    width: 100%;
+    padding: 0.5em 1em 0.3em 1em;
+    box-sizing: border-box;
+		background: rgba(255, 255, 255, 0.05);
+		border-radius: 8px;
+		text-align: center;
+  }
+  .todos :global(input) {
+    border: 1px solid transparent;
+  }
+  .todos :global(input:focus-visible) {
+    box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.1);
+    border: 1px solid #ff3e00 !important;
+    outline: none;
+  }
 </style>
 
 <svelte:head>
