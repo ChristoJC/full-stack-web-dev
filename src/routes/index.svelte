717f79ac7b4e9c3b92fb3ef64bf11a662ac90135
ChristoJC <christocheng2238@gmail.com>
2022-03-11 00:05:11 +0000

    fixes draft

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,14 +3,35 @@
     const title = "Todo";
 </script>
 
+<style>
+    .todos{
+        width: 100%;
+        max-width: 42rem;
+        margin: 4rem auto 0 auto;
+    }
+
+    .new{
+        margin-bottom: 0.5rem;
+    }
+
+    .new input{
+        
+    }
+</style>
+
 <svelte:head>
     <title>{title}</title>
 </svelte:head>
 
-<h1>{title}</h1>
+<div class="todos">
+    
+    <h1>{title}</h1>
+    
+    <form action="" method="" class="new">
+        <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add todo">
+    </form>
 
-<form action="">
-    <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add todo">
-</form>
-
-<TodoItem />
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+</div>
