45d01f33c31604a5237e75072fa77d5cce2ebdd6
ChristoJC <christocheng2238@gmail.com>
2022-03-12 22:40:38 +0000

    Added patch

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -81,7 +81,7 @@
         <button aria-label="Mark done/not done" class="toggle"></button>
     </form>
 
-    <form action="" method="" class="text">
+    <form action="/todos/{todo.uid}.json?_method=patch" method="post" class="text">
         <input type="text" value="{todo.text}">
         <button aria-label="Save todo" class="save"> </button>
     </form>
--- src/routes/todos/[uid].json.ts
+++ src/routes/todos/[uid].json.ts
@@ -5,3 +5,10 @@ import {api} from "./_api"
 export const del: RequestHandler = (request) => {
     return api(request);
   }
+
+  export const patch: RequestHandler  = ({request}) =>{
+    const formData = await request.formData();
+    return api(request, {
+      text: formData.get("text") as string,
+    }
+  }
