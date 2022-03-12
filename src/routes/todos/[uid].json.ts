c4251fb5f57e530a0c039da0b29b085753171169
ChristoJC <christocheng2238@gmail.com>
2022-03-12 23:14:02 +0000

    add patch end point - done

--- src/routes/todos/[uid].json.ts
+++ src/routes/todos/[uid].json.ts
@@ -6,9 +6,10 @@ export const del: RequestHandler = (request) => {
     return api(request);
   }
 
-  export const patch: RequestHandler  = ({request}) =>{
+  export const patch: RequestHandler  = async ({request}) =>{
     const formData = await request.formData();
     return api(request, {
       text: formData.get("text") as string,
-    }
+      done: formData.has("done") ? !! formData.has("done") : undefined
+    }) 
   }
